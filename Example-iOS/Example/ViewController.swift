c9b93975a44d50b4cb9115169092a50f2f336c12
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2017-05-17 11:43:04 +0200

    Remove memoryWarning

--- Example-iOS/Example/ViewController.swift
+++ Example-iOS/Example/ViewController.swift
@@ -21,12 +21,6 @@ class ViewController: UIViewController, HRMManagerDelegate {
         heartRateManager.enableDebugging()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
     func didConnect() {
         print("Did Connect to HRM")
     }
